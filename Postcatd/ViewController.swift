be66774975f7fa8354c9e87ecbbbd08985a7eac6
darnoldi <davearnoldi@Daves-iMac.local>
2014-09-11 13:41:21 +0200

    Updated to copy message to label

--- Postcatd/ViewController.swift
+++ Postcatd/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton)
+    {
+        //Code will evaluate when we press the button
+        messageLabel.hidden = false
+        messageLabel.text = messageTextField.text
+        messageTextField.text = ""
+       
+    }
+    
 
 }
 
